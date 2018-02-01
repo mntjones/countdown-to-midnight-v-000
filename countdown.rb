c30ad43c1dd8a3b2b750413f679c6c746db64d01
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-01 21:14:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,15 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    countdown_with_sleep(number)
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
   while number > 0
+    puts "#{number} SECOND(S)!"
     number -= 1
+    sleep(1)
   end
+  "HAPPY NEW YEAR!"
 end
